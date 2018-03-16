86915e1c7cbcd2ae2cdb84f6b8d45bed556010c3
Muhammad-Rehan-Farooq <36793916+Muhammad-Rehan-Farooq@users.noreply.github.com>
2018-03-16 21:44:31 +0500

    Add files via upload

---
+++ Lab03/Task 01/myMathFunc.c
@@ -0,0 +1,18 @@
+int isEqual(int x,int y)
+{
+   if(x==y)
+   {
+      return 1;
+   }
+   else
+   {
+      return -1;
+   }
+}
+int swap(int* a,int* b)
+{
+   *a=*a+*b;
+   *b=*a-*b;
+   *a=*a-*b;
+   return 0;
+}
